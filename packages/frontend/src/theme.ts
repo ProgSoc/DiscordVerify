3a64347212682d062631b8ad846acc883555251f
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-13 19:08:37 +1000

    system color mode

--- packages/frontend/src/theme.ts
+++ packages/frontend/src/theme.ts
@@ -2,7 +2,7 @@ import { ThemeConfig, extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   useSystemColorMode: true,
-  initialColorMode: "dark",
+  initialColorMode: "system",
 } satisfies ThemeConfig);
 
 export default theme;
